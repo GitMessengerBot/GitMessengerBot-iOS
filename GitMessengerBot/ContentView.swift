0a29e758f5a32104b63f0150cef51f7621d31912
jisungbin <ji@sungb.in>
2021-04-04 14:21:07 +0900

    First commit!

--- GitMessengerBot/ContentView.swift
+++ GitMessengerBot/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                Image("ic_launcher")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                Text("GitMessengerBot")
+                    .font(.title)
+                    .foregroundColor(Color.white)
+            }
+            VStack {
+                Text("© 2021 지성빈 & 구환. all rights reserved.")
+                    .font(.caption2)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.white)
+            }.frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .bottom
+            )
+        }
+        .padding()
+        .background(Color(UIColor(named: "Primary")!))
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity,
+            alignment: .center
+        )
     }
 }
 
