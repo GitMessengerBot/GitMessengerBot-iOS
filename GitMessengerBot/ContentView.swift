903195ad3fab92335a3053cd9449bfdd5a0218bb
jisungbin <ji@sungb.in>
2021-04-04 12:34:35 +0900

    Initial Commit

---
+++ GitMessengerBot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitMessengerBot
+//
+//  Created by Ji Sungbin on 2021/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitMessengerBot/GitMessengerBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitMessengerBotApp.swift
+//  GitMessengerBot
+//
+//  Created by Ji Sungbin on 2021/04/04.
+//
+
+import SwiftUI
+
+@main
+struct GitMessengerBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
